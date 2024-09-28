660c998a1559cd881eeedc1034ce03899197220e
LittleWhole <wholelittle@gmail.com>
2024-09-28 15:37:02 -0400

    adjust sizing

--- rizzlabs/app/page.tsx
+++ rizzlabs/app/page.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex min-h-screen">
-      <div className="flex w-100 flex-col p-4 bg-gray-900">
+      <div className="flex basis-1/4 w-100 flex-col p-4 bg-gray-900">
       <Button>Upload</Button>
       </div>
-      <div className="flex w-full flex-col p-4  bg-gray-800">
+      <div className="flex basis-3/4 w-full flex-col p-4  bg-gray-800">
       </div>
     </div>
   );
